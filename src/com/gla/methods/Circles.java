902a273351366e4fc8a03e51ba79455a4779d0de
ayush1237518 <ayushs9378@gmail.com>
2026-02-14 17:32:49 +0530

    access modifier circle

---
+++ src/com/gla/methods/Circles.java
@@ -0,0 +1,29 @@
+
+package com.gla.methods;
+
+public class Circles {
+
+    private double radius;
+
+    public Circle() {
+        this(1.0);
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public void display() {
+        System.out.println("Radius: " + radius);
+    }
+
+    public static void main(String[] args) {
+
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(5);
+
+        c1.display();
+        c2.display();
+    }
+    
+}
