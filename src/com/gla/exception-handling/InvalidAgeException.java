e7e1eecc2b4fffbc5948fc6042700de4602e27ee
Ayush sharma <ayushs9378@gmail.com>
2026-03-24 20:23:47 +0530

    Delete src/com/gla/exception-handling/InvalidAgeException.java

--- src/com/gla/exception-handling/InvalidAgeException.java
+++
@@ -1,8 +0,0 @@
-
-package com.gla.exception-handling.problem3;
-
-public class InvalidAgeException extends Exception {
-    public InvalidAgeException(String message) {
-        super(message);
-    }
-}
